4f59f71856731165c6bf6f9699aac932f1cb5a66
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-12-11 10:34:22 +0530

    Delegate and multicast delegate

---
+++ DelegateDemo/DelegateDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DelegateDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Delegate Demo");
+            Operation obj = new Operation(Demo.Addition);
+            //Console.WriteLine("Addition is = {0}",obj(23,27));
+            MulticastDelegate.ImplementDelegate();
+            Console.ReadKey();
+        }
+    }
+}
